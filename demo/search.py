71fa6f5ae2da95ae912b31cca71ff9ff25b69803
David Sullivan <dsullivan0190@gmail.com>
2017-12-15 10:52:24 +0000

    Updated search.py with non ltr query for testing

--- demo/search.py
+++ demo/search.py
@@ -1,10 +1,11 @@
-baseQuery = {
+baseLtrQuery = {
   "query": {
       "match": {
           "_all": "test"
        }
    },
   "rescore": {
+      "window_size": 1000,
       "query": {
         "rescore_query": {
             "sltr": {
@@ -18,14 +19,28 @@ baseQuery = {
    }
 }
 
+baseQuery = {
+  "query": {
+      "match": {
+          "_all": "test"
+       }
+   }
+}
+
 def ltrQuery(keywords, modelName):
     import json
-    baseQuery['rescore']['query']['rescore_query']['sltr']['model'] = model
+    baseLtrQuery['rescore']['query']['rescore_query']['sltr']['model'] = model
+    baseLtrQuery['query']['match']['_all'] = keywords
+    baseLtrQuery['rescore']['query']['rescore_query']['sltr']['params']['keywords'] = keywords
+    print("%s" % json.dumps(baseLtrQuery))
+    return baseLtrQuery
+
+
+def query(keywords):
+    import json
     baseQuery['query']['match']['_all'] = keywords
-    baseQuery['rescore']['query']['rescore_query']['sltr']['params']['keywords'] = keywords
     print("%s" % json.dumps(baseQuery))
     return baseQuery
-
 
 if __name__ == "__main__":
     import configparser
@@ -38,9 +53,13 @@ if __name__ == "__main__":
 
     es = Elasticsearch(esUrl, timeout=1000)
     model = "test_6"
+    results = None
     if len(argv) > 2:
         model = argv[2]
-    results = es.search(index='tmdb', doc_type='movie', body=ltrQuery(argv[1], model))
+        results = es.search(index='tmdb', doc_type='movie', body=ltrQuery(argv[1], model))
+    else:
+        results = es.search(index='tmdb', doc_type='movie', body=query(argv[1]))
+
     for result in results['hits']['hits']:
-             print(result['_source']['title'])
+                 print(result['_source']['title'] + " : " + str(result["_source"]["id"]))
 
